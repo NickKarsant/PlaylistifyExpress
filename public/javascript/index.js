d10522a69633487adb03a8b773775d17cfc2da00
Nicholas Karsant <nkarsant@gmail.com>
2021-02-20 11:56:52 -0800

    add toggleLiked visual functionality, green heart

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,31 +1,43 @@
-// toggle Hamgburger menu
-$("span#hamburger").click(function() {
-  toggleHamburger();
-});
+$(document).ready(function() {
+  console.log("ready!");
 
-function toggleHamburger() {
-  var menu = document.getElementById("navbarText");
-  if (menu.style.display === "none") {
-    menu.style.display = "block";
-  } else {
-    menu.style.display = "none";
-  }
-}
-
-// click menu link 
-$("#navbarText").on('click', function() {
-  $("a.nav-link").click(function() {
-    $("#navbarText").css("display", "none");
+  // toggle Hamgburger menu
+  $("span#hamburger").click(function() {
+    toggleHamburger();
   });
-});
 
+  function toggleHamburger() {
+    var menu = document.getElementById("navbarText");
+    if (menu.style.display === "none") {
+      menu.style.display = "block";
+    } else {
+      menu.style.display = "none";
+    }
+  }
 
+  $("#navbarText").on("click", function() {
+    $("a.nav-link").click(function() {
+      $("#navbarText").css("display", "none");
+    });
+  });
 
-
-var elements = document.getElementsByClassName("like");
-
-var addToLikedSongs = function() {};
-
-for (var i = 0; i < elements.length; i++) {
-  elements[i].addEventListener("click", addToLikedSongs);
+  // heart/like button toggle
+function toggleLiked() {
+  if (this.style.color === "") {
+    this.style.color = "green";
+    var isLiked = true
+  } else {
+    this.style.color = "";
+    var isLiked= false
+  }
+  return isLiked;
 }
+
+$("i.like").on('click', toggleLiked);
+
+
+
+
+
+
+});

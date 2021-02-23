9799f4a5ff0f97108bc839018ba951b3c2022e8d
Nicholas Karsant <nkarsant@gmail.com>
2021-02-22 17:44:14 -0800

    adding flash, not working

--- index.js
+++ index.js
@@ -57,6 +57,13 @@ seedDB();
 app.use(session(sessionConfig));
 app.use(flash());
 
+app.use((req,res, next) => {
+  res.locals.currentUser = req.user;
+  res.locals.success = req.flash('success');
+  res.locals.error = req.flash('error');
+  next();
+})
+
 app.use((req, res, next) => {
   res.locals.success = req.flash('success');
   res.locals.error = req.flash('error');
@@ -70,12 +77,7 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-app.use((req,res, next) => {
-  res.locals.currentUser = req.user;
-  res.locals.success = req.flash('success');
-  res.locals.error = req.flash('error');
-  next();
-})
+
 
 app.use('/playlists', playlistRoutes)
 app.use('/', userRoutes)
